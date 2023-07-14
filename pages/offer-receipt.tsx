a38562805d6a9bbfa8c65d1bc68e38964352e36a
Rachelh7 <yulanh7@gmail.com>
2023-07-14 12:08:36 +1000

    new offer receipt page

---
+++ pages/offer-receipt.tsx
@@ -0,0 +1,95 @@
+import React, { useState, FormEvent } from "react";
+import Layout from "../src/components/layout";
+import utilStyles from "../src/styles/utils.module.scss";
+import { useRouter } from "next/router";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Button } from "react-bootstrap";
+import QRCodeGenerator from "../utils/QRCodeUtils";
+import Link from "next/link";
+
+export default function Post() {
+  const router = useRouter();
+  // const uniqueId = router.query.uniqueId;
+  console.log(router.query);
+  const [uniqueId, setUniqueId] = useState("");
+  const newUrl = uniqueId ? `/opt-out?uniqueId=${uniqueId}` : `/opt-out`;
+  const [errors, setErrors] = useState<{
+    uniqueId?: string;
+  }>({});
+
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors: { uniqueId?: string } = {};
+    if (uniqueId.trim() === "") {
+      newErrors.uniqueId = "Unique ID is required"
+      isValid = false;
+    }
+    setErrors(newErrors);
+    return isValid;
+  }
+
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+
+    e.preventDefault();
+    if (validateForm()) {
+      // Form is valid, perform form submission logic here
+      router.push(newUrl)
+    }
+
+  }
+
+  return (
+    <Layout title="OFFER RECEIPT" logo="/images/logo.jpg">
+      <form onSubmit={handleSubmit}>
+        <Container>
+          <Row>
+            <Col sm="12" md="7">
+              <QRCodeGenerator url={newUrl} className="qrcode80" />
+              <p
+                className={`${utilStyles.text} ${utilStyles.lightText} ${utilStyles.flexCenter} ${utilStyles.pT10px}`}
+              >
+                <Link href={newUrl}>
+                  {newUrl}
+                </Link>
+
+              </p>
+            </Col>
+            <Col sm="12" md="5" className={`${utilStyles.pT30px}`}>
+              <div className={utilStyles.pB10px}>
+                <label htmlFor="firstName" className={`${utilStyles.pB10px} form-label`}>
+                  Unique ID:
+                </label>
+                <input
+                  type="text"
+                  className={`form-control ${errors.uniqueId && 'is-invalid'}`}
+                  id="firstName"
+                  value={uniqueId}
+                  onChange={(e) => setUniqueId(e.target.value)}
+                />
+                {errors.uniqueId && (
+                  <div className="invalid-feedback">{errors.uniqueId}</div>
+                )}
+              </div>
+            </Col>
+          </Row>
+        </Container>
+        <div
+          className={`${utilStyles.text} ${utilStyles.pB10px} ${utilStyles.textCenter} ${utilStyles.pT30px}`}
+        >
+          Lorem ipsum is placeholder text commonly used in the graphic, print, and
+          publishing industries for previewing layouts and visual mockups. Lorem
+          ipsum is placeholder text commonly used in the graphic, print, and
+          publishing industries for previewing layouts and visual mockups.
+        </div>
+        <div className={utilStyles.textCenter}>
+          <Button variant="primary" type="submit" className={utilStyles.button}>
+            Submit
+          </Button>
+        </div>
+      </form>
+    </Layout>
+  );
+}
