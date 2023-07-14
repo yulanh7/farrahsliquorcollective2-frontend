1a52f393a5310e4217a80bc03c285c3114689498
Rachelh7 <yulanh7@gmail.com>
2023-07-14 11:43:47 +1000

    migrate layout from pre project

---
+++ src/components/layout.tsx
@@ -0,0 +1,42 @@
+import Head from "next/head";
+import styles from "./layout.module.scss";
+import utilStyles from "../styles/utils.module.scss";
+import Link from "next/link";
+import PropTypes from "prop-types";
+
+export default function Layout({ children, title, logo }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+      </Head>
+      <header className={styles.header}>
+        <>
+          <Link href="/" className={styles.logoBox}>
+            <img src={logo} alt="logo" className={styles.logo} />
+          </Link>
+          <h1 className={utilStyles.headingLg}>{title}</h1>
+        </>
+      </header>
+      <main className={styles.pageContainer}>{children}</main>
+      <footer className={`${utilStyles.footerContainer} ${utilStyles.pT10px}`}>
+        <Link href="https://4block.com.au/">
+          <div>www.4block.com.au</div>
+        </Link>
+        <div className={`${utilStyles.textSm} `}>
+          Copyright © 2023 – All rights reserved
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+  title: PropTypes.node.isRequired,
+  logo: PropTypes.node.isRequired,
+};
