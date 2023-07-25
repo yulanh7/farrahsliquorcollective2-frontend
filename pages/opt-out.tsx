a6b82b7fdcf44333cd1fe45aa8d4d71537fa361d
Rachelh7 <yulanh7@gmail.com>
2023-07-25 14:50:31 +1000

    add radio input in opt-out page and change the style of radio input

--- pages/opt-out.tsx
+++ pages/opt-out.tsx
@@ -1,19 +1,29 @@
 import Layout from "../src/components/layout";
-import React, { useState, FormEvent } from "react";
+import React, { useState, FormEvent, ChangeEvent } from "react";
 import utilStyles from "../src/styles/utils.module.scss";
 import { Button } from "react-bootstrap";
 import Link from "next/link";
 
 export default function Post() {
+  const [optOutSelection, setOptOutSelection] = useState('optOutFromOffer');
+  const handleOptionChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setOptOutSelection(e.target.value);
+  };
   const [email, setEmail] = useState("");
   const [optOut, setOptOut] = useState(false);
   const [errors, setErrors] = useState<{
     email?: string;
+    optOutSelection?: string;
   }>({});
 
   const validateForm = () => {
     let isValid = true;
-    const newErrors: { email?: string } = {};
+    const newErrors: { email?: string, optOutSelection?: string } = {};
+    // Form validation
+    if (!optOutSelection) {
+      newErrors.optOutSelection = "Please select an option to opt out."
+      isValid = false;
+    }
     if (email.trim() === "") {
       newErrors.email = "Email is required"
       isValid = false;
@@ -43,7 +53,7 @@ export default function Post() {
   }
 
   return (
-    <Layout title="OPT OUT" logo="/images/logo.jpg">
+    <Layout title="OPT OUT" logo="/images/logo.jpg" topSubTitle="Unsubscribe">
       {
         optOut ? (
           <div>
@@ -60,24 +70,79 @@ export default function Post() {
           </div>
         ) : (
 
-          <form className={utilStyles.flexTop} onSubmit={handleSubmit}>
-            <div className={`${utilStyles.pR5px} ${utilStyles.pB10px}`}>
+          <form onSubmit={handleSubmit} className={utilStyles.flexColCenter}>
+            <div className={`${utilStyles.flexColStart} ${utilStyles.pB20px}`}>
+              <label className="customRadio">
+                <input
+                  type="radio"
+                  name="optOut"
+                  value="optOutFromOffer"
+                  checked={optOutSelection === 'optOutFromOffer'}
+                  onChange={handleOptionChange}
+                />
+                <span className="checkmark"></span> {/* Pseudo-element for the custom checkbox appearance */}
+                Opt out from this offer
+              </label>
+              <label className="customRadio">
 
-              <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                width="300px"
-                className={`form-control ${errors.email && 'is-invalid'}`}
-                onChange={(e) => { setEmail(e.target.value) }}
-              />
-              {errors.email && (
-                <div className="invalid-feedback">{errors.email}</div>
+                <input
+                  type="radio"
+                  name="optOut"
+                  value="optOutFromFLC"
+                  checked={optOutSelection === 'optOutFromFLC'}
+                  onChange={handleOptionChange}
+                />
+                <span className="checkmark"></span> {/* Pseudo-element for the custom checkbox appearance */}
+                Opt out of {`Farrah's`} Liquor Collective
+              </label>
+              <label className="customRadio">
+
+                <input
+                  type="radio"
+                  name="optOut"
+                  value="optOutFrom4block"
+                  checked={optOutSelection === 'optOutFrom4block'}
+                  onChange={handleOptionChange}
+                />
+                <span className="checkmark"></span> {/* Pseudo-element for the custom checkbox appearance */}
+                Opt out of 4block altogether
+              </label>
+              <label className="customRadio">
+
+                <input
+                  type="radio"
+                  name="optOut"
+                  value="optOutFromAll"
+                  checked={optOutSelection === 'optOutFromAll'}
+                  onChange={handleOptionChange}
+                />
+                <span className="checkmark"></span> {/* Pseudo-element for the custom checkbox appearance */}
+                Opt out altogether
+              </label>
+              <div className={`${errors.optOutSelection && 'is-invalid'}`}></div>
+              {errors.optOutSelection && (
+                <div className="invalid-feedback">{errors.optOutSelection}</div>
               )}
             </div>
-            <Button variant="primary" type="submit" className={utilStyles.button}>
-              UNSUBSCRIBE
-            </Button>
+            <div className={utilStyles.flexTop}>
+              <div className={`${utilStyles.pR5px} ${utilStyles.pB10px}`}>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  value={email}
+                  width="300px"
+                  className={`form-control ${errors.email && 'is-invalid'}`}
+                  onChange={(e) => { setEmail(e.target.value) }}
+                />
+                {errors.email && (
+                  <div className="invalid-feedback">{errors.email}</div>
+                )}
+              </div>
+              <Button variant="primary" type="submit" className={utilStyles.button}>
+                UNSUBSCRIBE
+              </Button>
+            </div>
+
           </form>
         )
       }
