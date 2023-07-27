3bda6e139164c349be1a8b22839644cc987207bd
Rachelh7 <yulanh7@gmail.com>
2023-07-27 13:24:37 +1000

    change offerSlice Text

--- store/offerSlice.ts
+++ store/offerSlice.ts
@@ -2,13 +2,13 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { AppThunk } from "./types";
 import { getOffersData } from "../api/api";
 
-interface UserState {
+interface OfferState {
   loading: boolean;
   data: any;
   error: string | null;
 }
 
-const initialState: UserState = {
+const initialState: OfferState = {
   loading: false,
   data: null,
   error: null,
@@ -18,33 +18,33 @@ const offerSlice = createSlice({
   name: "offer",
   initialState,
   reducers: {
-    getUserStart(state) {
+    getOfferStart(state) {
       state.loading = true;
       state.error = null;
     },
-    getUserSuccess(state, action: PayloadAction<any>) {
+    getOfferSuccess(state, action: PayloadAction<any>) {
       state.loading = false;
       state.data = action.payload;
       state.error = null;
     },
-    getUserFailure(state, action: PayloadAction<string>) {
+    getOfferFailure(state, action: PayloadAction<string>) {
       state.loading = false;
       state.error = action.payload;
     },
   },
 });
 
-export const { getUserStart, getUserSuccess, getUserFailure } =
+export const { getOfferStart, getOfferSuccess, getOfferFailure } =
   offerSlice.actions;
 
 export default offerSlice.reducer;
 
 export const fetchUserData = (): AppThunk => async (dispatch) => {
   try {
-    dispatch(getUserStart());
+    dispatch(getOfferStart());
     const response = await getOffersData(); // API call using axios
-    dispatch(getUserSuccess(response));
+    dispatch(getOfferSuccess(response));
   } catch (error) {
-    dispatch(getUserFailure((error as Error).message));
+    dispatch(getOfferFailure((error as Error).message));
   }
 };
