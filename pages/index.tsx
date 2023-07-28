368e47675e42f2ee292f1a3882d23d0123caf6b9
Rachelh7 <yulanh7@gmail.com>
2023-07-28 12:13:10 +1000

    fix url text in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   const dispatch = useAppDispatch();
   const { userInfo } = useSelector((state: RootState) => state.user);
   const [subscriptionData, setSubscriptionData] = useState<PushSubscriptionJSON | null | undefined>(null);
-  const url = process.env.NEXT_PUBLIC_HOME_URL || '';
+  const url = `${process.env.NEXT_PUBLIC_HOME_URL}/detail` || '/detail';
   useEffect(() => {
     const setupPushNotification = async () => {
       const subscription = JSON.stringify(await run());
