df5380056632e78661edcea2a479cec165d799f3
Rachelh7 <yulanh7@gmail.com>
2023-07-28 13:50:13 +1000

    delete unused code in app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,13 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { AppProps } from 'next/app';
 import "../src/styles/globals.scss";
 import "bootstrap/dist/css/bootstrap.css";
 import { Provider } from 'react-redux';
 import store from '../store';
-import { run } from '../lib/notification'; // Import the run function from the notification.ts file
 
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  // useEffect(() => {
-  //   run(); // Call the run function to register the service worker and set up push notifications
-  // }, []);
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
