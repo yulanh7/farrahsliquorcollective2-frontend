3d100c6b92939b7458a54bb3972b6570b814fb5a
Rachelh7 <yulanh7@gmail.com>
2023-08-15 09:10:11 +1000

    change badge img size and position

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -3,8 +3,8 @@ import utilStyles from "../styles/utils.module.scss";
 export default function Badge() {
   return (
     <div className={utilStyles.badge}>
-      Securely
-      <div>Sign. 01</div>
+      {/* Securely
+      <div>Sign. 01</div> */}
     </div>
   );
 }
