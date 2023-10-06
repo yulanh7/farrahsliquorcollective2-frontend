5bdc3e69166ed677b551f5ac9d049e113c128282
chappiewu <37008392+chappiewuzefan@users.noreply.github.com>
2023-10-06 15:07:49 +1100

    add opt in in sw

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,7 @@
 /// <reference lib="webworker" />
 
 let unsubscribeURL = "";
+let viewOfferURL = "";
 
 self.addEventListener("install", function (_) {
   self.skipWaiting();
@@ -19,17 +20,26 @@ self.addEventListener("push", async function (event) {
   data.body ??= "No data sent by server";
   data.title ??= "No title sent by server";
   data.url ??= "https://4block.com.au/debug";
-  unsubscribeURL = data.usuburl ?? "https://4block.com.au/unsubscribe";
+  unsubscribeURL =
+    data.usuburl ?? "https://farrahsliquorcollective2.com/opt-out";
+  viewOfferURL = "https://farrahsliquorcollective2.com/detail";
   // Ensuring that there's always the option to unsubscribe, no matter what the server is sending.
-  data.actions.push({
+  data.actions.push(
+    {
+      action: 'view-offer',
+      type: 'button',
+      title: '👀 Opt In'
+    },
+    {
     action: "unsubscribe",
     type: "button",
     title: "👎 Unsubscribe",
-  });
+    }
+  );
 
   const notificationOptions = {
     body: data.body,
-    icon: "/images/4Block-512x512.png",
+    // icon: "/images/4Block-512x512.png",
     badge: "/images/4Block-72x72.png",
     data: {
       url: data.url,
@@ -52,7 +62,7 @@ self.addEventListener("notificationclick", function (event) {
     case "view-offer":
       console.log("Viewing offer");
       if (event.notification.data && event.notification.data.url) {
-        clickResponsePromise = clients.openWindow(event.notification.data.url);
+        clickResponsePromise = clients.openWindow(viewOfferURL);
       }
       //event.notification.close();
       break;
@@ -74,23 +84,26 @@ self.addEventListener("activate", function (event) {
 
 self.addEventListener("pushsubscriptionchange", function (event) {
   event.waitUntil(
-    fetch("https://farrahsliquorcollective2.com:3008/user/pushsubscriptionchange", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        old_endpoint: event.oldSubscription
-          ? event.oldSubscription.endpoint
-          : null,
-        new_endpoint: event.newSubscription
-          ? event.newSubscription.endpoint
-          : null,
-        new_p256dh: event.newSubscription
-          ? event.newSubscription.toJSON().keys.p256dh
-          : null,
-        new_auth: event.newSubscription
-          ? event.newSubscription.toJSON().keys.auth
-          : null,
-      }),
-    })
+    fetch(
+      "https://farrahsliquorcollective2.com:3008/user/pushsubscriptionchange",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          old_endpoint: event.oldSubscription
+            ? event.oldSubscription.endpoint
+            : null,
+          new_endpoint: event.newSubscription
+            ? event.newSubscription.endpoint
+            : null,
+          new_p256dh: event.newSubscription
+            ? event.newSubscription.toJSON().keys.p256dh
+            : null,
+          new_auth: event.newSubscription
+            ? event.newSubscription.toJSON().keys.auth
+            : null,
+        }),
+      }
+    )
   );
 });
